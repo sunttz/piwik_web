00b803e0582c6272c0aa1027bbfd68b7aa7e9925
suntt <suntt@11wlw.com>
2017-08-01 17:19:41 +0800

    概览页面-top10受访页面、top10入口页面

--- common/js/siteSummary.js
+++ common/js/siteSummary.js
@@ -14,11 +14,15 @@ $(document).ready(function(){
 	var visitTendencyChart = null;
 	init_visit();
 	ajax_visit();
+	// 加载Top10受访页面
+	top10Visit();
+	// 加载Top10入口页面
+	top10Entry();
 	
 });
 // 图表自适应
 window.onresize = function(){
-  visitTendencyChart.resize();
+	visitTendencyChart.resize();
 }
 
 // 今日流量
@@ -49,25 +53,30 @@ function getTodayFlow(){
 // 时间切换
 function changeDate(date){
 	$("#dateDiv .btn").removeClass("active");
-$("#dateDiv .btn[data="+date+"]").addClass("active");
-$("#date").val(date);
+	$("#dateDiv .btn[data="+date+"]").addClass("active");
+	$("#date").val(date);
+	// 加载访客趋势图
 	ajax_visit();
+	// 加载Top10受访页面
+	top10Visit();
+	// 加载Top10入口页面
+	top10Entry();
 }
 // 访客趋势图start
 // 初始化访客趋势图
 function init_visit(){
-visitTendencyChart = echarts.init(document.getElementById('visitTendency'));
-var option = {
-    title: {},
-    tooltip: {trigger: 'axis',},
-    legend: {bottom : '6%'},
-    grid: {left: '8%',right: '8%',top: '8%',containLabel: true},
-    toolbox: {feature: {}},
-    xAxis: {type: 'category',boundaryGap: false},
-    yAxis: {type: 'value'},
-    series: []
-};
-visitTendencyChart.setOption(option);
+	visitTendencyChart = echarts.init(document.getElementById('visitTendency'));
+	var option = {
+	    title: {},
+	    tooltip: {trigger: 'axis',},
+	    legend: {bottom : '6%'},
+	    grid: {left: '8%',right: '8%',top: '8%',containLabel: true},
+	    toolbox: {feature: {}},
+	    xAxis: {type: 'category',boundaryGap: false},
+	    yAxis: {type: 'value'},
+	    series: []
+	};
+	visitTendencyChart.setOption(option);
 }
 //加载访客趋势图
 function ajax_visit(){
@@ -82,22 +91,22 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			datas.push(data[k].nb_pageviews);
-		}
-		option = {
-			legend: {data:["浏览量(PV)"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}'}},
-	    		series:[{name:'浏览量(PV)',type:'line',stack: '浏览量(PV)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				datas.push(data[k].nb_pageviews);
+			}
+			option = {
+				legend: {data:["浏览量(PV)"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}'}},
+		    		series:[{name:'浏览量(PV)',type:'line',stack: '浏览量(PV)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
 	// 访客数(UV)
@@ -106,22 +115,22 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			datas.push(data[k]);
-		}
-		option = {
-			legend: {data:["访客数(UV)"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}'}},
-	    		series:[{name:'访客数(UV)',type:'line',stack: '访客数(UV)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				datas.push(data[k]);
+			}
+			option = {
+				legend: {data:["访客数(UV)"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}'}},
+		    		series:[{name:'访客数(UV)',type:'line',stack: '访客数(UV)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
 	// 访问
@@ -130,22 +139,22 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			datas.push(data[k]);
-		}
-		option = {
-			legend: {data:["访问"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}'}},
-	    		series:[{name:'访问',type:'line',stack: '访问',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				datas.push(data[k]);
+			}
+			option = {
+				legend: {data:["访问"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}'}},
+		    		series:[{name:'访问',type:'line',stack: '访问',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
 	// 用户数
@@ -154,22 +163,22 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			datas.push(data[k]);
-		}
-		option = {
-			legend: {data:["用户数"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}'}},
-	    		series:[{name:'用户数',type:'line',stack: '用户数',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				datas.push(data[k]);
+			}
+			option = {
+				legend: {data:["用户数"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}'}},
+		    		series:[{name:'用户数',type:'line',stack: '用户数',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
 	// 跳出率
@@ -178,28 +187,28 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			var br = data[k];
-			if(br instanceof Array){
-				br = "0";
-			}else{
-				br = br.substr(0,br.length-1);
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				var br = data[k];
+				if(br instanceof Array){
+					br = "0";
+				}else{
+					br = br.substr(0,br.length-1);
+				}
+				datas.push(br);
 			}
-			datas.push(br);
-		}
-		option = {
-			legend: {data:["跳出率"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value + '%';}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}%'}},
-	    		series:[{name:'跳出率',type:'line',stack: '跳出率',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			option = {
+				legend: {data:["跳出率"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value + '%';}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}%'}},
+		    		series:[{name:'跳出率',type:'line',stack: '跳出率',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
 	// 平均访问时长(秒)
@@ -208,96 +217,120 @@ function ajax_visit(){
 		ajax_jsonp(piwik_url, param, function(data){
 			data = eval(data);
 			var categories = new Array();
-		var datas = new Array();
-		for(var k in data){
-			categories.push(k);
-			var at = data[k];
-			if(at instanceof Array){
-				at = 0;
+			var datas = new Array();
+			for(var k in data){
+				categories.push(k);
+				var at = data[k];
+				if(at instanceof Array){
+					at = 0;
+				}
+				datas.push(at);
 			}
-			datas.push(at);
-		}
-		option = {
-			legend: {data:["平均访问时长(秒)"]},
-			tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
-	    		xAxis: {data:categories},
-	    		yAxis: {axisLabel:{formatter: '{value}'}},
-	    		series:[{name:'平均访问时长(秒)',type:'line',stack: '平均访问时长(秒)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
-	            areaStyle: {normal: {color:'#87CEEB'}},data:datas
-	    		}]
-		};
-		visitTendencyChart.hideLoading();
-		visitTendencyChart.setOption(option);
+			option = {
+				legend: {data:["平均访问时长(秒)"]},
+				tooltip:{formatter:function(params){return params[0].name + ' <br/>' + params[0].seriesName + ": " + params[0].value;}},
+		    		xAxis: {data:categories},
+		    		yAxis: {axisLabel:{formatter: '{value}'}},
+		    		series:[{name:'平均访问时长(秒)',type:'line',stack: '平均访问时长(秒)',itemStyle:{normal:{color: '#87CEEB',lineStyle:{color:'#87CEEB'}}},
+		            areaStyle: {normal: {color:'#87CEEB'}},data:datas
+		    		}]
+			};
+			visitTendencyChart.hideLoading();
+			visitTendencyChart.setOption(option);
 		});
 	}
-	/**
-	var param_visits = "module=API&method=VisitsSummary.getVisits&idSite="+idSite+"&period=day&date="+date+"&format=json&token_auth="+t+"&translateColumnNames=1";
-	var param_uniqueVisitors = "module=API&method=VisitsSummary.getUniqueVisitors&idSite="+idSite+"&period=day&date="+date+"&format=json&token_auth="+t;
-	var urls = new Array();
-	urls.push(encodeURI(param_visits));
-	urls.push(encodeURI(param_uniqueVisitors));
-	var param = {module:"API",method:"API.getBulkRequest",format:"json",urls:urls};
-	visitTendencyChart.showLoading();
-	ajax_jsonp(piwik_url, param, function(data){
-		data = eval(data);
-	var categories = new Array();
-	var datas = new Array();
-	var visit_data = data[0];
-	for(var cat in visit_data){
-		categories.push(cat);
-		datas.push(visit_data[cat]);
-	}
-	var uniqueVisitors_data = data[1];
-	var categories1 = new Array();
-	var datas1 = new Array();
-	for(var cat1 in uniqueVisitors_data){
-		categories1.push(cat1);
-		datas1.push(uniqueVisitors_data[cat1]);
-	}
-	var option = {
-		legend: {
-        		data:['访问',"唯一访客"]
-    		},
-    		xAxis: {
-    			data:categories
-    		},
-    		series:[{
-    			name:'访问',
-            type:'line',
-            stack: '访问',
-            areaStyle: {normal: {}},
-            data:datas
-    		},{
-    			name:'唯一访客',
-            type:'line',
-            stack: '唯一访客',
-            areaStyle: {normal: {}},
-            data:datas1
-    		}]
-	};
-	visitTendencyChart.hideLoading();
-	visitTendencyChart.setOption(option);
-	});
-	*/
 }
 
 // 浏览量、访客数按钮点击事件
 function visitTendency_btn1(index){
 	$("#visitTendency_btn_text").text("其他");
-$("#visitTendencyIndex").val(index);
-$("#btn_group button").removeClass("active");
-$("#btn_"+index).addClass("active");
-// 刷新访客趋势图
+	$("#visitTendencyIndex").val(index);
+	$("#btn_group button").removeClass("active");
+	$("#btn_"+index).addClass("active");
+	// 刷新访客趋势图
 	ajax_visit();
 }
 // 访客趋势图其他按钮点击事件
 function visitTendency_btn2(index){
 	$("#visitTendency_btn_text").text($("#btn_"+index).text());
-$("#visitTendencyIndex").val(index);
-$("#btn_group button").removeClass("active");
-$("#btn_other").addClass("active");
-// 刷新访客趋势图
+	$("#visitTendencyIndex").val(index);
+	$("#btn_group button").removeClass("active");
+	$("#btn_other").addClass("active");
+	// 刷新访客趋势图
 	ajax_visit();
 }
 // 访客趋势图end
+
+// Top10受访页面start
+function top10Visit(){
+	var date = $("#date").val(); // 日期
+	var p_1 = "module=API&method=Actions.getPageUrls&idSite="+idSite+"&period=range&date="+date+"&format=json&token_auth="+t+"&filter_limit=10&filter_sort_column=nb_hits&filter_sort_order=desc";
+	var p_2 = "module=API&method=MultiSites.getOne&idSite="+idSite+"&period=range&date="+date+"&format=JSON&token_auth="+t;
+	var urls = new Array();
+	urls.push(encodeURI(p_1));
+	urls.push(encodeURI(p_2));
+	var p = getBulkRequestParam(urls);
+	ajax_jsonp(piwik_url,p,function(data){
+		data = eval(data);
+		var totalPv = data[1].nb_pageviews;
+		var pages = data[0];
+		var tbodyHtml = "";
+		for(var k in pages){
+			var page = pages[k];
+			var label = page.label;
+			// label最长52字符
+			if(getStrLength(label) > 52){
+				label = cutStr(label,52);
+			}
+			var url = page.url;
+			var pv = page.nb_hits;
+			var prop = Math.round(pv / totalPv * 10000) / 100.00 + "%";
+			tbodyHtml += "<tr>";
+			tbodyHtml += "<td title='"+url+"'><a target='_blank' href='"+url+"'>"+label+"</a></td>";
+			tbodyHtml += "<td align='center'>"+pv+"</td>";
+			tbodyHtml += "<td><div title='"+prop+"' style='background-color:#87CEEB; width:"+prop+";'>"+prop+"</div></td>";
+			tbodyHtml += "</tr>";
+		}
+		$("#top10_visit_tbody").html(tbodyHtml);
+	});
+}
+// Top10受访页面end
    
+// Top10入口页面start
+function top10Entry(){
+	var date = $("#date").val(); // 日期
+	var p_1 = "module=API&method=Actions.getEntryPageUrls&idSite="+idSite+"&period=range&date="+date+"&format=JSON&token_auth="+t;
+	var p_2 = "module=API&method=Actions.getEntryPageUrls&idSite="+idSite+"&period=range&date="+date+"&format=JSON&token_auth="+t+"&filter_limit=10&filter_sort_column=entry_nb_visits&filter_sort_order=desc";
+	var urls = new Array();
+	urls.push(encodeURI(p_1));
+	urls.push(encodeURI(p_2));
+	var p = getBulkRequestParam(urls);
+	ajax_jsonp(piwik_url,p,function(data){
+		data = eval(data);
+		var totalPv = 0; // 入口页面浏览量总和
+		var totalEntry = data[0];
+		for(var i in totalEntry){
+			totalPv += parseInt(totalEntry[i].entry_nb_visits);
+		}
+		var entrys = data[1];
+		var tbodyHtml = "";
+		for(var j in entrys){
+			var entry = entrys[j];
+			var label = entry.label;
+			// label最长52字符
+			if(getStrLength(label) > 52){
+				label = cutStr(label,52);
+			}
+			var url = entry.url;
+			var pv = entry.entry_nb_visits;
+			var prop = Math.round(pv / totalPv * 10000) / 100.00 + "%";
+			tbodyHtml += "<tr>";
+			tbodyHtml += "<td title='"+url+"'><a target='_blank' href='"+url+"'>"+label+"</a></td>";
+			tbodyHtml += "<td align='center'>"+pv+"</td>";
+			tbodyHtml += "<td><div title='"+prop+"' style='background-color:#87CEEB; width:"+prop+";'>"+prop+"</div></td>";
+			tbodyHtml += "</tr>";
+		}
+		$("#top10_entry_tbody").html(tbodyHtml);
+	});
+}
+// Top10入口页面end
